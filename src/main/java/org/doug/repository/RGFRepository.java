eb49427e5282a58712b0567f444208242c07dfee
Wesley00s <wesley300rodrigues@gmail.com>
2025-03-18 21:29:23 -0300

    fix: fix correct dependency import

--- src/main/java/org/doug/repository/RGFRepository.java
+++ src/main/java/org/doug/repository/RGFRepository.java
@@ -2,10 +2,10 @@ package org.doug.repository;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.vertx.core.json.JsonArray;
+import io.vertx.core.json.JsonObject;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.inject.Inject;
-import jakarta.json.JsonArray;
-import jakarta.json.JsonObject;
 import org.doug.client.RGFClient;
 import org.doug.dto.RGFDto;
 import org.eclipse.microprofile.rest.client.inject.RestClient;
@@ -25,8 +25,26 @@ public class RGFRepository {
     @Inject
     ObjectMapper objectMapper;
 
-    public List<RGFDto> getRGF(int anoExercicio, String inPeriodicidade, int numeroPeriodo, String codigoTipoDemonstrativo, String nomeAnexo, String codigoEsfera, int idEnte) {
-        JsonObject response = rgfClient.getRGF(anoExercicio, inPeriodicidade, numeroPeriodo, codigoTipoDemonstrativo, nomeAnexo, codigoEsfera, idEnte);
+    public List<RGFDto> getRGF(
+            int anoExercicio,
+            String inPeriodicidade,
+            int numeroPeriodo,
+            String codigoTipoDemonstrativo,
+            String nomeAnexo,
+            String codigoEsfera,
+            String codigoPoder,
+            int idEnte
+    ) {
+        JsonObject response = rgfClient.getRGF(
+                anoExercicio,
+                inPeriodicidade,
+                numeroPeriodo,
+                codigoTipoDemonstrativo,
+                nomeAnexo,
+                codigoEsfera,
+                codigoPoder,
+                idEnte
+        );
 
         if (!response.containsKey("items")) {
             return Collections.emptyList();
@@ -35,7 +53,8 @@ public class RGFRepository {
         JsonArray itemsArray = response.getJsonArray("items");
 
         List<Map<String, Object>> items = itemsArray.stream()
-                .map(jsonValue -> objectMapper.convertValue(jsonValue, new TypeReference<Map<String, Object>>() {}))
+                .map(jsonValue -> objectMapper.convertValue(jsonValue, new TypeReference<Map<String, Object>>() {
+                }))
                 .toList();
 
         return items.stream()
