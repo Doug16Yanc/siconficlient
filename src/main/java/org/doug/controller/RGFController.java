ea918f4ae5485ec19a6eb19f58761d3bc93b23e1
Wesley00s <wesley300rodrigues@gmail.com>
2025-03-18 21:24:14 -0300

    refactor: refactor RGF controller to download csv data

--- src/main/java/org/doug/controller/RGFController.java
+++ src/main/java/org/doug/controller/RGFController.java
@@ -9,9 +9,15 @@ import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.QueryParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVPrinter;
 import org.doug.dto.RGFDto;
 import org.doug.service.RGFService;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 @Path("/rgf")
@@ -26,22 +32,79 @@ public class RGFController {
     Template rgf;
 
     @GET
-    public Response getTabela(
+    public Response getRGFController(
             @QueryParam("an_exercicio") int anoExercicio,
             @QueryParam("in_periodicidade") String inPeriodicidade,
             @QueryParam("nr_periodo") int numeroPeriodo,
             @QueryParam("co_tipo_demonstrativo") String codigoTipoDemonstrativo,
             @QueryParam("no_anexo") String nomeAnexo,
             @QueryParam("co_esfera") String codigoEsfera,
-            @QueryParam("id_ente") int idEnte
-    ) {
+            @QueryParam("co_poder") String codigoPoder,
+            @QueryParam("id_ente") int idEnte) {
+
         List<RGFDto> items = rgfService.getRGF(
                 anoExercicio, inPeriodicidade, numeroPeriodo, codigoTipoDemonstrativo,
-                nomeAnexo, codigoEsfera, idEnte
+                nomeAnexo, codigoEsfera, codigoPoder, idEnte
         );
 
-        String html = rgf.data("items", items).render();
+        return Response.ok(rgf.data("items", items).render()).build();
+    }
 
-        return Response.ok(html).build();
+
+    @GET
+    @Path("/csv")
+    @Produces("text/csv")
+    public Response getRGFAsCsv(
+            @QueryParam("an_exercicio") int anoExercicio,
+            @QueryParam("in_periodicidade") String inPeriodicidade,
+            @QueryParam("nr_periodo") int numeroPeriodo,
+            @QueryParam("co_tipo_demonstrativo") String codigoTipoDemonstrativo,
+            @QueryParam("no_anexo") String nomeAnexo,
+            @QueryParam("co_esfera") String codigoEsfera,
+            @QueryParam("co_poder") String codigoPoder,
+            @QueryParam("id_ente") int idEnte) {
+
+        List<RGFDto> items = rgfService.getRGF(
+                anoExercicio, inPeriodicidade, numeroPeriodo, codigoTipoDemonstrativo,
+                nomeAnexo, codigoEsfera, codigoPoder, idEnte
+        );
+
+        try (ByteArrayOutputStream out = new ByteArrayOutputStream();
+             CSVPrinter printer = new CSVPrinter(
+                     new OutputStreamWriter(out, StandardCharsets.UTF_8),
+                     CSVFormat.DEFAULT
+                             .withHeader(
+                                     "Exercicio", "Periodo", "Periodicidade",
+                                     "Instituicao", "CodIbge", "UF", "CodPoder", "Populacao", "Anexo",
+                                     "Rotulo", "Coluna", "CodConta", "Conta", "Valor"
+                             ))) {
+
+            for (RGFDto item : items) {
+                printer.printRecord(
+                        item.exercicio(),
+                        item.periodo(),
+                        item.periodicidade(),
+                        item.instituicao(),
+                        item.codIbge(),
+                        item.uf(),
+                        item.coPoder(),
+                        item.populacao(),
+                        item.anexo(),
+                        item.rotulo(),
+                        item.codConta(),
+                        item.conta(),
+                        item.valor()
+                );
+            }
+
+            printer.flush();
+
+            return Response.ok(out.toByteArray())
+                    .header("Content-Disposition", "attachment; filename=\"rgf-data.csv\"")
+                    .build();
+
+        } catch (IOException e) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).build();
+        }
     }
 }
